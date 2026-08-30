ca0c61c18b156252d5ca7070b731092a1af079c2
Phil Seeley <phil.seeley@gmail.com>
2026-08-30 16:20:46 +0200

    We connect and read data on a timed basis.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { ServerAPI, Plugin, Path, PathValue } from '@signalk/server-api'
 
-import { SerialPort, SerialPortMock } from 'serialport'
+import { SerialPort } from 'serialport'
 import { DelimiterParser} from '@serialport/parser-delimiter'
 
 interface PluginSettings {
+  updateRate: number;
   serialDevice: string;
   baudRate: number;
   wgID: string;
@@ -12,19 +13,9 @@ interface PluginSettings {
   pvName: string;
 }
 
-function parse(item: string, name: string, div: number) {
-  if(item.startsWith(name)) {
-    item = item.slice(name.length);
-    var value = parseInt(item, 10) / div;
-    return value;
-  }
-
-  return null;
-}
-
-var port: SerialPort | undefined;
-
 module.exports = (app: ServerAPI): Plugin => {
+  var timer: NodeJS.Timeout | undefined;
+
   const plugin: Plugin = {
     id: 'signalk-marlec-plugin',
     name: 'Marlec Charge Controller',
@@ -32,54 +23,24 @@ module.exports = (app: ServerAPI): Plugin => {
     start: (settings: PluginSettings, restartPlugin) => {
       app.debug(`Start settings="${JSON.stringify(settings)}"`)
 
-      port = new SerialPort({path: settings.serialDevice, baudRate: settings.baudRate});
-      var parser = port.pipe(new DelimiterParser({delimiter: 'WG12000'}))
-      parser.on('data', (chunk: Buffer) => {
-        var data = chunk.toString('ascii');app.debug(data);
-        // var deltas: Map<string, any>[] = [];
-        var values: PathValue[] = [];
-
-        for(const item of data.split(' ')) {
-          var value;
-          if(settings.wgID !== undefined) {
-            value = parse(item, 'WGV', 100);
-            if(value !== null) { values.push({path: `electrical.windGenerator.${settings.wgID}.voltage` as Path, value: value}); continue; };
-            value = parse(item, 'WGI', 100);
-            if(value !== null) { values.push({path: `electrical.windGenerator.${settings.wgID}.current` as Path, value: value}); continue; };
-            value = parse(item, 'WGon', 1);
-            if(value !== null) { values.push({path: `electrical.windGenerator.${settings.wgID}.state` as Path, value: {code: value, message: value===0?'off':'on'}}); continue; };
-          }
-
-          if(settings.pvID !== undefined) {
-            value = parse(item, 'PVV', 100);
-            if(value !== null) { values.push({path: `electrical.solar.${settings.pvID}.voltage` as Path, value: value}); continue; };
-            value = parse(item, 'PVI', 100);
-            if(value !== null) { values.push({path: `electrical.solar.${settings.pvID}.current` as Path, value: value}); continue; };
-            value = parse(item, 'PVon', 1);
-            if(value !== null) { values.push({path: `electrical.solar.${settings.pvID}.state` as Path, value: {code: value, message: value===0?'off':'on'}}); continue; };
-          }
-        }
-
-        app.debug(values);
-
-        app.handleMessage(plugin.id, {
-            updates: [{
-              values: values
-            }]
-          })
-      })
+      setTimer(settings);
     },
 
     stop: () => {
       app.debug(`Stop`)
-      port?.close();
-      port = undefined;
+      clearTimeout(timer);
+      timer = undefined;
     },
     
     schema: {
       type: 'object',
-      required: ['serialPort'], //TODO what does this do?
+      required: ['updateRate', 'serialPort'], //TODO what does this do?
       properties: {
+        updateRate: {
+          type: 'number',
+          title: 'Seconds between updates - Required',
+          default: 5
+        },
         serialDevice: {
           type: 'string',
           title: 'The USB serial port device - Required',
@@ -110,6 +71,79 @@ module.exports = (app: ServerAPI): Plugin => {
     }
   }
 
-  return plugin
+  return plugin;
+
+  function setTimer(settings: PluginSettings) {
+    timer = setTimeout(() => {readData(settings);}, settings.updateRate*1000);
+  }
+
+  function readData(settings: PluginSettings) {
+    var readCount = 0;
+    var port = new SerialPort({path: settings.serialDevice, baudRate: settings.baudRate});
+    var parser = port.pipe(new DelimiterParser({delimiter: 'WG12000', includeDelimiter: true}));
+    parser.on('data', (chunk: Buffer) => {
+      // We wait for multiple buffers as the first one may only be partial.
+      ++readCount;
+
+      if(readCount > 2) {
+        readCount = 0;
+        var data = chunk.toString('ascii');
+        app.debug(data);
+
+        updateData(settings, data);
+
+        parser.destroy();
+        port.close();
+
+        setTimer(settings);
+      }
+    });
+  }
+
+  function parse(item: string, name: string, div: number) {
+    if(item.startsWith(name)) {
+      item = item.slice(name.length);
+      var value = parseInt(item, 10) / div;
+      return value;
+    }
+
+    return null;
+  }
+
+  function updateData(settings: PluginSettings, data: string) {
+    var values: PathValue[] = [];
+
+    if(settings.wgID !== undefined && settings.wgName !== undefined) values.push({path: `electrical.windGenerator.${settings.wgID}.name` as Path, value: settings.wgName});
+    if(settings.pvID !== undefined && settings.pvName !== undefined) values.push({path: `electrical.solar.${settings.pvID}.name` as Path, value: settings.pvName});
+
+    for(const item of data.split(' ')) {
+      var value;
+      if(settings.wgID !== undefined) {
+        value = parse(item, 'WGV', 100);
+        if(value !== null) { values.push({path: `electrical.windGenerator.${settings.wgID}.voltage` as Path, value: value}); continue; };
+        value = parse(item, 'WGI', 100);
+        if(value !== null) { values.push({path: `electrical.windGenerator.${settings.wgID}.current` as Path, value: value}); continue; };
+        value = parse(item, 'WGon', 1);
+        if(value !== null) { values.push({path: `electrical.windGenerator.${settings.wgID}.state` as Path, value: {code: value, message: value===0?'off':'on'}}); continue; };
+      }
+
+      if(settings.pvID !== undefined) {
+        value = parse(item, 'PVV', 100);
+        if(value !== null) { values.push({path: `electrical.solar.${settings.pvID}.voltage` as Path, value: value}); continue; };
+        value = parse(item, 'PVI', 100);
+        if(value !== null) { values.push({path: `electrical.solar.${settings.pvID}.current` as Path, value: value}); continue; };
+        value = parse(item, 'PVon', 1);
+        if(value !== null) { values.push({path: `electrical.solar.${settings.pvID}.state` as Path, value: {code: value, message: value===0?'off':'on'}}); continue; };
+      }
+    }
+
+    app.debug(values);
+
+    app.handleMessage(plugin.id, {
+      updates: [{
+        values: values
+      }]
+    })
+  }
 }
 
